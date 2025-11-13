18ca0d77ba7362cb1ca650dab2f6ef02772fa50e
William Zhong <anotherperson51@gmail.com>
2025-11-12 18:45:56 -0600

    Added parallax calcs

--- src/main/java/frc/robot/Subsystems/GamePieceFinder/GamePieceFinder.java
+++ src/main/java/frc/robot/Subsystems/GamePieceFinder/GamePieceFinder.java
@@ -1,6 +1,8 @@
 package frc.robot.Subsystems.GamePieceFinder;
 
 import static edu.wpi.first.units.Units.*;
+import static frc.robot.Subsystems.Vision.VisionConstants.ROBOT_TO_FRONT_RIGHT_CAMERA_ROTATION;
+import static frc.robot.Subsystems.Vision.VisionConstants.ROBOT_TO_FRONT_RIGHT_CAMERA_TRANSLATION;
 
 import java.util.Queue;
 import java.util.concurrent.atomic.AtomicReference;
@@ -8,13 +10,18 @@ import java.util.concurrent.atomic.AtomicReference;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.Time;
 
 public class GamePieceFinder {
 
     private static AtomicReference<GamePieceFinder> instance;
     
-    private Queue<GamePieceParallaxSample> parllaxSamples;
+    private Queue<GamePieceParallaxSample> parallaxSamples;
     private Queue<GamePieceVisionSample> visionSamples;
 
     private Pose2d estimate;
@@ -61,7 +68,7 @@ public class GamePieceFinder {
     public void addDrivePose(Pose2d pose, Time timestamp) {
         for (GamePieceVisionSample t : visionSamples) {
             if (Math.abs(t.timestamp.in(Milliseconds) - timestamp.in(Milliseconds)) < ALLOWED_TIMESTAMP_DEVIATION.in(Milliseconds)) {
-                parllaxSamples.add(new GamePieceParallaxSample(pose, timestamp, t.visionSample));
+                parallaxSamples.add(new GamePieceParallaxSample(pose, timestamp, t.visionSample));
                 visionSamples.remove(t);
                 continue;
             }
@@ -75,10 +82,50 @@ public class GamePieceFinder {
 
     private void updateEstimate() {
         // Parallax Calc Here
+        GamePieceParallaxSample firstTempSample = parallaxSamples.poll(); //TODO: Am I good to remove the samples here?
+        GamePieceParallaxSample secondTempSample = parallaxSamples.poll();
+
+        //I need the "first" sample to have the smaller rotation so that I can set it as 0 and go off of that for calculations
+        //TODO: Logic will mess up if rotation can be negative
+        //TODO: Probably better way to implement this logic lol
+        GamePieceParallaxSample firstSample = firstTempSample.robotPose.getRotation().getDegrees() < secondTempSample.robotPose.getRotation().getDegrees() ? firstTempSample : secondTempSample;
+        GamePieceParallaxSample secondSample = firstTempSample.robotPose.getRotation().getDegrees() > secondTempSample.robotPose.getRotation().getDegrees() ? firstTempSample : secondTempSample;
+
+        //TODO: Need to make sure there are no weird singularities or smth happening here
+        Angle deltaYaw = Degrees.of(secondSample.robotPose.getRotation().minus(firstSample.robotPose.getRotation()).getDegrees());
+
+        Translation2d firstCameraPos = ROBOT_TO_FRONT_RIGHT_CAMERA_TRANSLATION.toTranslation2d();
+        //TODO: Need to check if this rotates the right way - should be to the left
+        Translation2d secondCameraPos = firstCameraPos.rotateAround(Translation2d.kZero, Rotation2d.fromDegrees(deltaYaw.in(Degrees)));
+
+        Translation2d vecBtwnCameraPos = firstCameraPos.minus(secondCameraPos);
+        
+        Angle cameraRot = ROBOT_TO_FRONT_RIGHT_CAMERA_ROTATION.getMeasureZ();
+        Angle angleOffset = Radians.of(Math.atan2(firstCameraPos.getY(), firstCameraPos.getX()));
+        Angle firstYaw = Degrees.of(firstSample.visionSample.yaw);
+        Angle secondYaw = Degrees.of(secondSample.visionSample.yaw);
+
+        //TODO: There's some redudant math here, need to come back and simplify/clean it up later
+        Angle alpha = Radians.of(Math.atan2(vecBtwnCameraPos.getY(), vecBtwnCameraPos.getX()));
+        Angle beta = Degrees.of(90 - alpha.in(Degrees));
+        Angle A = Degrees.of(cameraRot.in(Degrees) - firstYaw.in(Degrees) - alpha.in(Degrees));
+        Angle B = Degrees.of(360 - (cameraRot.in(Degrees) - secondYaw.in(Degrees) + angleOffset.in(Degrees) + ((180 - deltaYaw.in(Degrees))/2 - beta.in(Degrees))));
+        Angle C = Degrees.of(180 - A.in(Degrees) - B.in(Degrees));
+
+        //TODO: Need to make sure coordinate system matches (im assuming right is positive and up is positive)
+        Distance b = Meters.of((vecBtwnCameraPos.getDistance(Translation2d.kZero)/Math.sin(C.in(Radians)))*Math.sin(A.in(Radians)));
+        Translation2d robotToObject = new Translation2d(
+            -b.in(Meters)*Math.cos(A.in(Radians) + alpha.in(Radians)) + firstCameraPos.getMeasureX().in(Meters),
+            b.in(Meters)*Math.sin(A.in(Radians) + alpha.in(Radians)) + firstCameraPos.getMeasureY().in(Meters)
+        );
+
+        //This should cancel the rotation of the bot and make the estimated pose of the object have 0 rotation
+        //TODO: If it doesn't actually do that then fix it
+        estimate = firstSample.robotPose.plus(new Transform2d(robotToObject, new Rotation2d(-firstSample.robotPose.getX(), -firstSample.robotPose.getY())));
     }
 
     public void Periodic() { 
-        if (parllaxSamples.size() > 1) {
+        if (parallaxSamples.size() > 1) {
             updateEstimate();
         }
     }
